d6316bd1818c608f542a5e8d82113a2d7fd5a961
malknaor <malknaor@gmail.com>
2019-08-04 13:31:07 +0300

    Setting up BrowseRouter And creation of necessary components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>
-            App
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/stream/new" component={StreamCreate} />
+                    <Route path="/stream/edit" component={StreamEdit} />
+                    <Route path="/stream/delete" component={StreamDelete} />
+                    <Route path="/stream/show" component={StreamShow} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
